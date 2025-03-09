b40963d1e025a00ef16faaac6492be960ee82f3e
aljosueb <46324456+aljosueb@users.noreply.github.com>
2025-03-08 20:58:57 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,13 +1,21 @@
-
-
 def main():
+    m, n = map(int, input().split())
+    H = list(map(int, input().split()))
+    D = list(map(int, input().split()))
 
-	m, n = map(int, input().split())
-	H = list(map(int, input().split()))
-	D = list(map(int, input().split()))
+    ganancia_total = 0
+    comida_usada = -1
 
-	
+    for i in range(m):
+        if H[i] == n:
+            comida_usada = i
+            break
+
+    for i in range(m):
+        if i != comida_usada:
+            ganancia_total += D[i]
+
+    print(ganancia_total)
+
 if __name__ == '__main__':
-	main()
-
-
+    main()
